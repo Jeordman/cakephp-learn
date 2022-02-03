dbba8fa4e8b45319fea4973e97496c0fee2b020c
jeordman <callisterjeordin@gmail.com>
2022-02-03 15:13:33 -0500

    post routes setup with logging

---
+++ src/Controller/PostsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+// always make controller plural
+class PostsController extends AppController
+{
+  public function initialize(): void
+  {
+    // log
+    var_dump('PostsController::initialize()');
+  }
+
+  public function index()
+  {
+    die('<h1>PostsController::index()</h1>');
+  }
+}
