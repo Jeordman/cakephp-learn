33f61ba63c298181c437e20decd93120a94042f9
jeordman <callisterjeordin@gmail.com>
2022-02-04 14:42:33 -0500

    form validation

--- src/Model/Table/PostsTable.php
+++ src/Model/Table/PostsTable.php
@@ -3,11 +3,27 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Validation\Validator;
 
 class PostsTable extends Table
 {
-    public function initialize(array $config): void
-    {
-        $this->addBehavior('Timestamp');
-    }
+  public function initialize(array $config): void
+  {
+    $this->addBehavior('Timestamp');
+  }
+
+  public function validationDefault(Validator $validator): Validator
+  {
+    $validator
+      ->notEmpty('title')
+      ->requirePresence('title')
+      ->notEmpty('body')
+      ->requirePresence('body')
+      ->notEmpty('author')
+      ->requirePresence('author')
+      ->notEmpty('category')
+      ->requirePresence('category');
+
+    return $validator;
+  }
 }
